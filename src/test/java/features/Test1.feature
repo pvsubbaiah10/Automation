b192e7caad6e4afb3520f4f87c8409362192d6dc
pvsubbaiah10 <subbaiah.psl@gmail.com>
2025-05-26 02:01:01 +0530

    from Intelij

--- src/test/java/features/Test1.feature
+++ src/test/java/features/Test1.feature
@@ -1,5 +1,15 @@
 Feature: My first test
   Scenario: Launch new window
 
-    Given the web application "Flipkart" is launched in a NewWindow
-
+    Given the web application "Automation_Practice_Url" is launched in a NewWindow
+    
+    #And the user enters UserName "student" into the User textbox at the LoginPage page
+    #And the user enters Password "Password123" into the User textbox at the LoginPage page
+    #And the user clicks the "login" element at the LoginPage page
+    
+    #And the user close the browser
+    
+    
+    And the user enters UserName "student" into the "Username" textbox at the "LoginPage" page
+    
+    
--- src/test/java/stepDefinitions/LauncWindow.java
+++ src/test/java/stepDefinitions/LauncWindow.java
@@ -1,29 +1,56 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import utils.DriverSetup;
-import utils.Driverclass;
+import utils.DriverManager;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.Properties;
+import java.util.Set;
 
 public class LauncWindow {
 
-    public WebDriver driver;
-
-    Driverclass dc;
+   
+	 /*  ************** Launch the browser ************** */
     
     @Given("the web application {string} is launched in a NewWindow")
     public void Launch_NewWindow(String weburl) throws IOException {
-      
-    	dc = new Driverclass();
-    	dc.DriverManager(weburl);
-      
-    	
-    }
+          	    	
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\global.properties");
+        Properties prop = new Properties();
+        prop.load(fis);
+        
+        
+        Set<String> li =prop.stringPropertyNames();
+        
+        for(String s :li) { 	
+        	 if(s.equals(weburl)) {
+        		
+        		System.out.println("Launch "+ s +" Wrb_Page");
+        		String url = prop.getProperty(s);
+        		 
+        	    String browser = prop.getProperty("browser");
+                DriverManager.initializeDriver(browser, url);
+                break;
+        	 }else {
+        		 System.out.println("URL not found in properties file: " + weburl);
+        		 break;
+        	 }
+        }
+
+     }
+    
+        
+    
+    /*  ************** Close the browser ************** */
+    
+    @And("the user close the browser")
+    public void close_the_browser() {
+    	DriverManager.quitDriver();
+    }   
+    
+  
 
 
 

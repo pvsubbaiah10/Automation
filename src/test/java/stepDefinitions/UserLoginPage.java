8948a30ba6d4910cf74e7151bc83303d03168303
pvsubbaiah10 <subbaiah.psl@gmail.com>
2025-05-27 19:09:39 +0530

    from Intelij

--- src/test/java/stepDefinitions/UserLoginPage.java
+++ src/test/java/stepDefinitions/UserLoginPage.java
@@ -11,27 +11,21 @@ public class UserLoginPage {
 	   	   
 	   @And("^the user enters UserName \"([^\"]*)\" into the \"([^\"]*)\" textbox at the \"([^\"]*)\" page$")
 	   public void Enter_UserName(String username, String textboxFieldName, String pageClassName) throws IOException {
-		   
-	  
-		   
+		      
 	   LoadProperties.pageClass_LoadPage(username, textboxFieldName, pageClassName);
 	   
 	   }  
     
-	   
-	   
-	   
+  
 	   @And("^the user enters Password \"([^\"]*)\" into the \"([^\"]*)\" textbox at the \"([^\"]*)\" page$")
 	   public void Enter_Password(String password, String textboxFieldName, String pageClassName){
-	   
-		   
+	     
 			try {
 				LoadProperties.pageClass_LoadPage(password, textboxFieldName, pageClassName);
 			} catch (Exception e) {
 				
 				e.printStackTrace();
-			}
-		 
+			} 
 	   }
 	      
 
@@ -39,9 +33,7 @@ public class UserLoginPage {
 	   public void Click_button(String button, String pageClassName) {
 	   
 		    LoadProperties.pageClass_button(button, pageClassName);
-		   
-		   
-		   
+ 
 	   }
 	     
 	   

414dba3a78eba8a37c181b4a064b9bd5fb395f0f
pvsubbaiah10 <subbaiah.psl@gmail.com>
2025-05-30 00:53:51 +0530

    from Intelij

--- src/test/java/utils/EncryptUtils.java
+++ src/test/java/utils/EncryptUtils.java
@@ -6,7 +6,7 @@ import java.util.Base64;
 
 public class EncryptUtils {
 
-	private static final String KEY= "Ov3atJOEPsDdWtLo"; //Don't mention key like this in your class file.
+	private static final String KEY= "Ov3atJOEPsDdWtLo"; // Don't mention key like this in your class file.
     
 
     //private static final String KEY = System.getenv("ENCRYPTION_KEY"); // External key more secure it hides your key.
@@ -15,22 +15,26 @@ public class EncryptUtils {
         if (KEY == null || !(KEY.length() == 16 || KEY.length() == 24 || KEY.length() == 32)) {
             throw new IllegalArgumentException("ENCRYPTION_KEY must be 16, 24, or 32 characters for AES-128/192/256");
         }
-        Cipher cipher = Cipher.getInstance("AES");
+        
         SecretKeySpec keySpec = new SecretKeySpec(KEY.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-        byte[] encrypted = cipher.doFinal(data.getBytes());
-        return Base64.getEncoder().encodeToString(encrypted); // it gives encrypted value into string format.
+        byte[] encrypted = cipher.doFinal(data.getBytes()); // encoding given data with key.
+        String Es = Base64.getEncoder().encodeToString(encrypted); // it gives encrypted byte value into string format.
+        return Es; 
     }
 
     public static String Decode(String encryptedData) throws Exception {
         if (KEY == null || !(KEY.length() == 16 || KEY.length() == 24 || KEY.length() == 32)) {
             throw new IllegalArgumentException("ENCRYPTION_KEY must be 16, 24, or 32 characters for AES-128/192/256");
         }
-        Cipher cipher = Cipher.getInstance("AES");
+       
         SecretKeySpec keySpec = new SecretKeySpec(KEY.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, keySpec);
         byte[] decoded = Base64.getDecoder().decode(encryptedData);
-        return new String(cipher.doFinal(decoded));
+        String  Ds  =new String(cipher.doFinal(decoded)); //decoding given encryptedData with key and gives into string format.
+        return  Ds; 
     }
     
     

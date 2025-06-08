4c7ad1b5432600ade95141bdcc9d47226f5c49b0
pvsubbaiah10 <subbaiah.psl@gmail.com>
2025-06-08 23:41:58 +0530

    from Intelij

--- src/test/java/Runners/TestNGRunner.java
+++ src/test/java/Runners/TestNGRunner.java
@@ -1,14 +1,9 @@
 package Runners;
 
-
 import io.cucumber.testng.CucumberOptions;
-
-
 
 @CucumberOptions(tags = "@test1 or @test2")
 
 public class TestNGRunner extends BaseTestNGRunner {
-	
 
-	
 }
